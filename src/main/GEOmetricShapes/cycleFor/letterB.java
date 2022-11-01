724e29892e2070daf33caffe492739d27052a2a2
YERKANAT <93324789+iamerkanat@users.noreply.github.com>
2022-11-01 21:06:26 +0600

    letters B & E

---
+++ src/main/GEOmetricShapes/cycleFor/letterB.java
@@ -0,0 +1,22 @@
+package cycleFor;
+
+public class letterB {
+    public static void main(String[] args) {
+        int n =11;
+        int i, j;
+
+        for(i = 1; i <= n; i++){
+
+            for (j = 1; j <= n; j++) {
+                if(j==1 || j == 2 || i == 1 || i == n || (i == n/2+1 && j<=n-1))
+                    System.out.print("B");
+
+                else if(i!=1 && i!=n && j==n)
+                    System.out.print("B");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ src/main/GEOmetricShapes/cycleFor/letterE.java
@@ -0,0 +1,23 @@
+package cycleFor;
+
+import java.util.Scanner;
+
+public class letterE {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Could you enter your favourite number: ");
+        int n = scan.nextInt();
+
+        //int n =11;
+        int i, j;
+
+        for(i = 1; i <= n; i++){
+
+            for (j = 1; j <= n; j++) {
+                if(j==1 || j == 2 || i == 1 || i == n || i == n/2+1 && j<=n-1)
+                    System.out.print("E");
+            }
+            System.out.println("");
+        }
+    }
+}
